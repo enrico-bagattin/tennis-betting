b032eae7473ba1b414010ab1a92c00202a555959
Enrico Bagattin <e.bagattin@egeatech.com>
2020-03-14 20:57:08 +0100

    ADD: Dataset, loaded data

---
+++ dataPreparation.py
@@ -0,0 +1,11 @@
+def findOddsForRow (row, df):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        row   - Required  : selected row (Series)
+        df    - Required  : original dataframe (Dataframe)
+    """
+    col_list = ['AvgW', 'AvgL']
+    df.loc[(df['column_name'] >= A) & (df['column_name'] <= B)]
+
+    return row, df
